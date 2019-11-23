b6e138124981cb3c640baa3dda97302330d2b684
Fawno1 <maissaude.equipe@gmail.com>
2019-11-22 22:12:07 -0300

    Adicionado CarroService

--- src/main/java/br/com/fundatec/carro/api/CarroApi.java
+++ src/main/java/br/com/fundatec/carro/api/CarroApi.java
@@ -1,5 +1,6 @@
 package br.com.fundatec.carro.api;
 
+import br.com.fundatec.carro.service.CarroService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +10,16 @@ import java.util.List;
 @RestController
 public class CarroApi {
 
-    @GetMapping("Carros")
-    public List<String> getCarro() {
-        List<String>carros = Arrays.asList("Mustange",
-                "Fusca",
-                "Fumbica",
-                "Celta",
-                "Uno com escada");
+    private final CarroService carroService;
+    public CarroApi(CarroService carroService){
+        this.carroService = carroService;
+    }
+
+
+
+    @GetMapping("carros")
+    public List<String> getCarros() {
+        List<String>carros = carroService.listarCarros();
         return carros;
     }
 }

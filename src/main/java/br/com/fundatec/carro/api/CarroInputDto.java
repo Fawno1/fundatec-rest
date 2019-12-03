1c000d524dc50680add88aca0b90790c30567f11
Fawno1 <maissaude.equipe@gmail.com>
2019-12-02 21:34:58 -0300

    Validação de dados de entrada

--- src/main/java/br/com/fundatec/carro/api/CarroInputDto.java
+++ src/main/java/br/com/fundatec/carro/api/CarroInputDto.java
@@ -1,9 +1,41 @@
 package br.com.fundatec.carro.api;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+
 public class CarroInputDto {
 
+    @NotBlank(message = "O campo nome é obrigatório, gurizão.")
     private String nome;
+    @Pattern(regexp = "^[A-Z]{3}[0-9]{4}$", message = "Placa inválida")
+    @NotBlank(message = "O campo placa é obrigatório, meu confederado.")
     private String placa;
+
+    @Past(message = "Data de fabricação deve ser no passado!")
+    @NotNull(message = "O campo data de fabricação é obrigatório!")
+    private LocalDate dataFabricacao;
+
+    @NotNull(message = "O campo data do modelo é obrigatório!")
+    private LocalDate dataModelo;
+
+    public LocalDate getDataFabricacao() {
+        return dataFabricacao;
+    }
+
+    public void setDataFabricacao(LocalDate dataFabricacao) {
+        this.dataFabricacao = dataFabricacao;
+    }
+
+    public LocalDate getDataModelo() {
+        return dataModelo;
+    }
+
+    public void setDataModelo(LocalDate dataModelo) {
+        this.dataModelo = dataModelo;
+    }
 
     public String getNome() {
         return nome;
